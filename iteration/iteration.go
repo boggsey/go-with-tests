82e08cb0849c38c329ac5d2c0c1f04946d849f3b
boggsey <eric@mlemedia.com>
2022-10-05 11:56:50 -0400

    Adding benchmarks

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 )
 
-func Repeat(s string) string {
+const repeatCount = 5
+
+func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + s
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
 	}
 	return repeated
 }
